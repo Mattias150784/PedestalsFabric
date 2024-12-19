7c65b753e728a04035ca186d9cd24d05bba9f627
MattiasAye <Mattiasayenew@gmail.com>
2024-12-18 20:44:14 -0800

    Yeah that's right Fabric!!

---
+++ src/main/java/net/mattias/pedestal/PedestalModClient.java
@@ -0,0 +1,10 @@
+package net.mattias.pedestal;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PedestalModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/mattias/pedestal/PedestalsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mattias.pedestal;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PedestalsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
